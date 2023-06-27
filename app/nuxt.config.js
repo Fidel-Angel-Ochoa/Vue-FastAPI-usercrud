fbc919bfb01fcc35486d22d670191903bd027be4
fidel-angel-ochoa <fidel.angel.ochoa@gmail.com>
2023-06-26 21:43:37 -0700

    adding the 'buildDir:dist' in nuxt.config.js

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,10 +1,7 @@
 export default {
-    buildModules: [
-        "@nuxtjs/vuetify"
-    ],
-    modules: [
-        "@nuxtjs/axios" 
-    ],
-    // to import the components automatic
-    components: true
-}
+  buildDir: "dist", // this is the dir where the project is create in the host(cloudflare)
+  buildModules: ["@nuxtjs/vuetify"],
+  modules: ["@nuxtjs/axios"],
+  // to import the components automatic
+  components: true,
+};
