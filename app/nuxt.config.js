c717e48c5f03d8be73301486c1de6413b34977b7
fidel-angel-ochoa <fidel.angel.ochoa@gmail.com>
2023-06-27 21:12:30 -0700

    modified .gitignore, nuxt.config.js, package.json, to generate the project on nuxt

--- app/nuxt.config.js
+++ app/nuxt.config.js
@@ -1,5 +1,6 @@
 export default {
   buildDir: "dist", // this is the dir where the project is create in the host(cloudflare)
+  target: "static",
   buildModules: ["@nuxtjs/vuetify"],
   modules: ["@nuxtjs/axios"],
   // to import the components automatic
